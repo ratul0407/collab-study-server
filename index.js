68f9398436d58eb1cd127712623a0a24bf65bfcc
ratul0407 <rajaulislamratul12@gmail.com>
2025-01-28 01:02:21 +0600

    api to delete sessions

--- index.js
+++ index.js
@@ -253,6 +253,13 @@ async function run() {
         res.send(result);
       }
     );
+
+    app.delete("/session/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await sessionsCollection.deleteOne(query);
+      res.send(result);
+    });
     //update sessions status and price
     app.patch("/session/:id", verifyToken, verifyApproval, async (req, res) => {
       const id = req.params.id;
